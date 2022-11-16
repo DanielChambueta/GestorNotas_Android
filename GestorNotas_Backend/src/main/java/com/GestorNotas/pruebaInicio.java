cbf9701983223b3429733051bd04855f9a56f623
DanielChambueta <chambuetadaniel@gmail.com>
2022-11-16 00:43:26 -0500

    Inicializacion del Repositorio

---
+++ GestorNotas_Backend/src/main/java/com/GestorNotas/pruebaInicio.java
@@ -0,0 +1,11 @@
+package com.GestorNotas;
+
+public class pruebaInicio {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola mundo");
+
+	}
+
+}
